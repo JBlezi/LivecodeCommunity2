573a3c2c32d1055c8624d74890044caeacfb09b0
Julius Blezinger <jgoldappel@gmail.com>
2022-06-28 12:50:50 +0200

    get profile img from github if github url is present

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,26 +1,33 @@
+require 'open-uri'
+require 'json'
+
 class UserInformationsController < ApplicationController
 
-    def show
-        @user_information = UserInformation.find(params[:id])
-        authorize @user_information
-        @tickets = Ticket.where(user_id: @user_information.user_id)
-        @ticket_ids = @tickets.map(&:id)
-        @reviews = Review.select { |r| @ticket_ids.include?(r.ticket_id)}
-    end
+  def show
+    @user_information = UserInformation.find(params[:id])
+    authorize @user_information
+    @tickets = Ticket.where(user_id: @user_information.user_id)
+    @ticket_ids = @tickets.map(&:id)
+    @reviews = Review.select { |r| @ticket_ids.include?(r.ticket_id)}
 
-    def edit
-    end
+    url = "https://api.github.com/users/JBlezi"
+    open_url = URI.open(url).read
+    @api = JSON.parse(open_url)
+  end
 
-    def update
-      @user_information = UserInformation.find(params[:id])
-      @user_information.update(user_information_params)
-      redirect_to  user_information_path
-      authorize @user_information
-    end
+  def edit
+  end
 
-    private
+  def update
+    @user_information = UserInformation.find(params[:id])
+    @user_information.update(user_information_params)
+    redirect_to  user_information_path
+    authorize @user_information
+  end
 
-    def user_information_params
-     params.require(:user_information).permit(:first_name, :last_name, :bio, :github_url)
-    end
+  private
+
+  def user_information_params
+    params.require(:user_information).permit(:first_name, :last_name, :bio, :github_url)
+  end
 end
