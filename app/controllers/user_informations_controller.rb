45b3c181c3505a36f8bce0c77913644de07bc736
Julius Blezinger <jgoldappel@gmail.com>
2022-06-30 12:23:10 +0200

    messages display only messages that are meant for you and with the correct profile pic

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -1,6 +1,3 @@
-require 'open-uri'
-require 'json'
-
 class UserInformationsController < ApplicationController
 
   def show
@@ -9,12 +6,9 @@ class UserInformationsController < ApplicationController
     @tickets = Ticket.where(user_id: @user_information.user_id)
     @ticket_ids = @tickets.map(&:id)
     @reviews = Review.select { |r| @ticket_ids.include?(r.ticket_id)}
-
-    if @user_information.github_url
+    if @user_information.github_url != "https://github.com/"
       github_username = @user_information.github_url.split("/").last
-      url = "https://api.github.com/users/#{github_username}"
-      open_url = URI.open(url).read
-      @api = JSON.parse(open_url)
+      @gh_api = JSON.parse(URI.open("https://api.github.com/users/#{github_username}").read)
     end
   end
 
@@ -24,7 +18,7 @@ class UserInformationsController < ApplicationController
   def update
     @user_information = UserInformation.find(params[:id])
     @user_information.update(user_information_params)
-    redirect_to  user_information_path
+    redirect_to user_information_path
     authorize @user_information
   end
 
